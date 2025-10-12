deb4c35ee998617d07692d111c8b1a809a431804
benoitpf <39746026+benoitpf@users.noreply.github.com>
2025-10-12 22:31:22 +0200

    Create RSPWidget.js

---
+++ RSPWidget.js
@@ -0,0 +1,35 @@
+grist.ready({ requiredAccess: 'full' });
+
+const labelDansLeWidget = document.querySelector("#nom");
+let nomIssuDeLaTable = ""
+
+grist.onRecord(record => {
+  nomIssuDeLaTable = record.nom;
+  labelDansLeWidget.textContent = nomIssuDeLaTable;
+
+  let url = "https://apim-passerelle-klifbb-otl.omogen.in.cloe.education.gouv.fr/mesirh/dev/rsp/mes-qua-d1/api/externe/unite_structurelle_complet?identUs=";
+  url += record.nom;
+
+
+  fetch(url).then((response) => {
+    if (response.ok) {
+      response.json().then((data) => {
+              const results = data
+              if (!results) {
+                console.info("pas de data");
+              }
+              else{
+                document.getElementById('result').innerText = JSON.stringify(data);
+              }
+      })
+              
+    }
+    else {}
+
+  })
+
+});
+
+grist.onRecords(table => {
+  // Code qui sera executé lorsque les données dans la table ont changé.
+});
